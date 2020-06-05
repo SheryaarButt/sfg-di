3bca50bb660cfb9e4834664a2f292524d44bc909
Sheryaar <Sheryaar.butt@gmail.com>
2020-06-05 07:58:59 -0400

    Project Initialization

---
+++ src/test/java/com/sharueigo/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharueigo.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
